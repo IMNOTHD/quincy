461823e03131ceae9c165467e742fe7721400eef
IMNOTHD <1162719199@qq.com>
2020-07-03 12:52:44 +0800

    增加了一大堆东西

---
+++ src/main/java/com/piggy/quincy/component/MiraiApiHttpComponent.java
@@ -0,0 +1,13 @@
+package com.piggy.quincy.component;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 封装Mirai操作, 如发送信息等
+ * @author IMNOTHD
+ * @date 2020/6/29 17:13
+ */
+@Component
+public class MiraiApiHttpComponent {
+
+}
--- src/main/java/com/piggy/quincy/config/JacksonConfig.java
+++ src/main/java/com/piggy/quincy/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package com.piggy.quincy.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -28,15 +29,13 @@ public class JacksonConfig {
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
 
-        // 字段保留，将null值转为""
-        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
-            @Override
-            public void serialize(Object o, JsonGenerator jsonGenerator,
-                                  SerializerProvider serializerProvider)
-                    throws IOException, JsonProcessingException {
-                jsonGenerator.writeString("");
-            }
-        });
+        // 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
+        // Include.Include.ALWAYS 默认
+        // Include.NON_DEFAULT 属性为默认值不序列化
+        // Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量
+        // Include.NON_NULL 属性为NULL 不序列化,就是为null的字段不参加序列化
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
         return objectMapper;
     }
 }
